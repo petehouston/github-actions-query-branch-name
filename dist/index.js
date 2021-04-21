d37e0d3db4cd41e895bcf2bb4f1db3614dbf5f99
Pete Houston <contact@petehouston.com>
2021-04-21 16:12:19 -0500

    Try out with pull_request

--- dist/index.js
+++ dist/index.js
@@ -462,6 +462,8 @@ const core = __nccwpck_require__(375);
 
 async function run() {
     try {
+        // output name
+        const OutputGitBranchName = 'git_branch_name';
         // get input
         const name = core.getInput('name', { required: true })
         // current branch
@@ -469,7 +471,20 @@ async function run() {
         // export to variable
         core.exportVariable(name, branchName);
         // set output
-        core.setOutput('git_branch_name', branchName);
+        core.setOutput(OutputGitBranchName, branchName);
+
+        // check if this is a pull request
+        if (process.env.GITHUB_EVENT_NAME === 'pull_request') {
+            const baseBranchName = process.env.GITHUB_BASE_REF.split('/').slice(2).join('/');
+            // const baseBranchName = process.env.GITHUB_BASE_REF;
+            core.exportVariable(`${name}_BASE`, baseBranchName);
+            core.setOutput(`${OutputGitBranchName}_base`, baseBranchName);
+
+            const headBranchName = process.env.GITHUB_HEAD_REF.split('/').slice(2).join('/');
+            // const headBranchName = process.env.GITHUB_HEAD_REF;
+            core.exportVariable(`${name}_HEAD`, headBranchName);
+            core.setOutput(`${OutputGitBranchName}_head`, headBranchName);
+        }
     }
     catch (error) {
         core.setFailed(error.message);
